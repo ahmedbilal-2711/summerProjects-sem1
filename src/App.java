356a47bf90afc9292780769e9e58238639a57eb9
Ahmed Bilal Siddiqui <ahmedbilal6880@gmail.com>
2021-08-01 23:20:07 +0500

    password done

--- src/App.java
+++
@@ -1,7 +0,0 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World! sjdhjsdkjfkjdf");
-        System.out.println("Hello, World! sjdhjsf");
-        System.out.println("branch test");
-    }
-}
---
+++ src/mimmy/login.java
@@ -0,0 +1,45 @@
+
+// An intro page is created with an icon which is to be clicked and then will proceed forward
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.border.Border;
+import java.awt.event.*;
+import java.awt.*;
+
+public class login {
+
+    login() {
+        JFrame f = new JFrame();
+        f.getContentPane().setLayout(null);
+        JLabel l1 = new JLabel();
+        l1.setBackground(Color.decode("#037A94"));
+        l1.setSize(500, 500);
+        l1.setOpaque(true);
+        JButton icon = new JButton(new ImageIcon("images/Mimmys.png"));
+        // f.setLocationRelativeTo(null);
+        f.add(l1);
+        f.add(icon);
+        f.setSize(500, 500);
+        icon.setBorder(BorderFactory.createLineBorder(Color.black, 2));
+        icon.setSize(200, 200);
+        icon.setLocation(140, 140);
+        f.setVisible(true);
+        f.setDefaultCloseOperation(3);
+
+        icon.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // TODO Auto-generated method stub
+                f.dispose();
+                passcode p = new passcode();
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        login n = new login();
+    }
+}
